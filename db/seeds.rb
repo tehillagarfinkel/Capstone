8c47ca5a479de8974aebd27a6e8dd987377c7e17
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-07-24 20:55:51 -0500

    create seeds file, add secure password feature to user model, add column password_digest to user model, activate bcrypt gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,13 @@
+User.create!(name: "Tehilla", email: "tehilla@email.com", password: "password")
+User.create!(name: "Rachel", email: "rachel@email.com", password: "password")
+User.create!(name: "Ariella", email: "ariella@email.com", password: "password")
 
+Category.create!(name: "food")
+Category.create!(name: "home")
+Category.create!(name: "clothing")
+Category.create!(name: "travel")
+
+Task.create!(description: "fix bookshelf", user_id: 2, category_id: 2, completed: "FALSE", duration: 60, due_date: 20.days.from_now)
+Task.create!(description: "buy milk", user_id: 1, category_id: 1, completed: "FALSE", duration: 20, due_date: 5.days.from_now)
+Task.create!(description: "pick up dry cleaning", user_id: 2, category_id: 3, completed: "FALSE", duration: 30, due_date: 13.days.from_now)
+Task.create!(description: "plan weekly menu", user_id: 3, category_id: 1, completed: "FALSE", duration: 45, due_date: 3.days.from_now)
