b465dcde5957a83dc24995042f1728f0304448a7
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-07-28 14:59:56 -0500

    add users create action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,10 @@
 class Api::UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+    post "/users" => "users#create"
+  end
 end
